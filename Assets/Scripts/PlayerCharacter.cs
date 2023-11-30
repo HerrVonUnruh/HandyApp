ad2b4c556671df46675ce57d3ae6557aaab9025d
m4dm3rt <mert-cansin@hotmail.de>
2023-11-30 14:07:30 +0100

    [+] Added Move on Cube

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    public Rigidbody rb;
+    private Vector2 touchStartPosition;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Überprüfen, ob der Bildschirm berührt wird
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    touchStartPosition = touch.position;
+                    break;
+
+                case TouchPhase.Moved:
+                    // Berechne die Distanz, die der Finger auf dem Bildschirm zurückgelegt hat
+                    float touchDelta = touch.position.x - touchStartPosition.x;
+
+                    // Berechne die Bewegung basierend auf der Distanz und der Geschwindigkeit
+                    float move = Mathf.Clamp(touchDelta * Time.deltaTime * moveSpeed, -moveSpeed, moveSpeed);
+
+                    // Bewegung auf der x-Achse basierend auf der Touch-Bewegung
+                    rb.velocity = new Vector2(move, rb.velocity.y);
+                    break;
+
+                case TouchPhase.Ended:
+                case TouchPhase.Canceled:
+                    // Setze die Geschwindigkeit auf Null, wenn der Finger den Bildschirm nicht mehr berührt
+                    rb.velocity = new Vector2(0, rb.velocity.y);
+                    break;
+            }
+        }
     }
 }
